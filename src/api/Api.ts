49bba19bfddd26361d4548f32945a21ac89713f3
AaronCode-Art <arontc2003@gmail.com>
2026-06-12 17:35:25 -0500

    cambio de api

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8090/api',
+  baseURL: 'https://backendsyo.onrender.com/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
